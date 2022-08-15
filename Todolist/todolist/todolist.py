f5926d1ec83f7f936e885bf281e3fd488d1e46cc
Pravin <drkwolf52@gmail.com>
2022-08-16 03:20:33 +0530

    Add files via upload

--- Todolist/todolist/todolist.py
+++ Todolist/todolist/todolist.py
@@ -1,13 +1,17 @@
 # Write your code here
+from datetime import datetime, timedelta
+from sys import exit
+
+from colorama import init, Fore, Style
+from pyfiglet import figlet_format
 from sqlalchemy.ext.declarative import declarative_base   # used to give orm features to classes
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy import create_engine
-from datetime import datetime, timedelta
-from sys import exit
+
 
 Base = declarative_base()       # function return a DeclarativeMeta class which all model class should inherit
-
+init(autoreset=True)
 
 class Table(Base):
     __tablename__ = 'task'
@@ -22,7 +26,7 @@ class Table(Base):
 class TodoList:
     def __init__(self):
         # connect to the database
-        self.engine = create_engine('sqlite:///todo.db?check_same_thread=False')
+        self.engine = create_engine('sqlite:///data/todo.db?check_same_thread=False')
         Base.metadata.create_all(self.engine)   # create the table in the database
         Session = sessionmaker(bind=self.engine)
         self.session = Session()                # to access the database in orm
@@ -30,90 +34,97 @@ class TodoList:
 
     def all_tasks(self):
         rows = self.session.query(Table).order_by(Table.deadline).all()
-        print('All tasks:')
+        print(Style.BRIGHT + Fore.BLACK + '> All Tasks')
         if rows:
             for index, row in enumerate(rows, start=1):
-                print(f'{index}. {row.task}. {row.deadline.day} {row.deadline.strftime("%b")}')
+                print(f'{Fore.RED + str(index)}. {Fore.WHITE + row.task}. - {Fore.YELLOW + row.deadline.strftime("%d %b %Y")}')
         else:
-            print('Nothing to do!')
+            print('Nothing to do 🙂!')
         print()
 
     def today_tasks(self):
         rows = self.session.query(Table).filter(Table.deadline == self.today).all()
-        print(self.today.strftime('Today %d %b:'))
+        print(Style.BRIGHT + Fore.BLACK + self.today.strftime('> Today - %d %b %Y'))
         if rows:
             for index, row in enumerate(rows, start=1):
                 print(f'{index}. {row.task}')
         else:
-            print('Nothing to do!')
+            print('Nothing to Do 🙂!')
         print()
 
     def weeks_tasks(self):
+        print(Style.BRIGHT + Fore.BLACK + "> Week's Tasks Details")
         for i in range(7):
             day_date = self.today + timedelta(days=i)
             rows = self.session.query(Table).filter(Table.deadline == day_date).all()
-            print(day_date.strftime('%A %d %b:'))
+            print(Fore.LIGHTYELLOW_EX + day_date.strftime('%A %d %b'))
             if rows:
                 for index, row in enumerate(rows, start=1):
-                    print(f'{index}. {row.task}')
+                    print(f'{Fore.RED + str(index)}. {Fore.WHITE + row.task}')
             else:
-                print('Nothing to do!')
+                print('Nothing to Do 🙂!')
             print()
 
     def missed_tasks(self):
         rows = self.session.query(Table).filter(Table.deadline < self.today).order_by(Table.deadline).all()
-        print('Missed tasks:')
+        print(Style.BRIGHT + Fore.BLACK + '> Missed Tasks')
         if rows:
             for index, row in enumerate(rows, start=1):
-                print(f'{index}. {row.task}. {row.deadline.day} {row.deadline.strftime("%b")}')
+                print(f'{Fore.RED + str(index)}. {Fore.WHITE + row.task}. - {Fore.YELLOW + row.deadline.strftime("%d %b %Y")}')
         else:
-            print('Nothing to do!')
+            print('Nothing to Do 🙂!')
         print()
 
     def add_task(self):
-        print('Enter task')
-        task_todo = input()
-        print('Enter deadline(%Y-%m-%d)')
         try:
-            deadline = datetime.strptime(input(), '%Y-%m-%d').date()
-        except ValueError as e:
-            print(e)
-            print('No deadline was set!')
+            print(Style.BRIGHT + Fore.BLACK + '> Enter Task Details')
+
+            task_todo = input('Task Description: ')
+            task_deadline = input('Task Deadline[yyyy-mm-dd]: ')
+
+            deadline = datetime.strptime(task_deadline, '%Y-%m-%d').date()
+        except ValueError:
+            print(Fore.RED + 'Invalid or No deadline was set!')
+            print()
         else:
             new_row = Table(task=task_todo, deadline=deadline)
-
-        self.session.add(new_row)
-        self.session.commit()
-        print('The task has been added!\n')
+            self.session.add(new_row)
+            self.session.commit()
+            print(Fore.GREEN + 'Task has been added!')
+            print()
 
     def delete_task(self):
         rows = self.session.query(Table).order_by(Table.deadline).all()
-        print('Choose the number of the task you want to delete:')
+        print(Style.BRIGHT + Fore.BLACK + '> Choose the number of the task you want to delete:')
         if rows:
             for index, row in enumerate(rows, start=1):
-                print(f'{index}. {row.task}. {row.deadline.day} {row.deadline.strftime("%b")}')
+                print(f'{Fore.RED + str(index)}. {Fore.WHITE + row.task}. - {Fore.YELLOW + row.deadline.strftime("%d %b %Y")}')
 
-            del_index = int(input()) - 1
+            print()
+            del_index = int(input('Task: ')) - 1
             self.session.delete(rows[del_index])
             self.session.commit()
-            print('The task has been deleted!')
+            print(Fore.GREEN + 'The task has been deleted!')
         else:
-            print('Nothing to delete')
+            print('Nothing to Delete 🙂!')
         print()
 
     def interface(self):
         """Provide interface for user to view, add tasks"""
+
+        TITLE = figlet_format('TODO TASKS', 'slant')
         while True:
-            print("1) Today's tasks")
-            print("2) Week's tasks")
-            print("3) All tasks")
-            print("4) Missed tasks")
-            print("5) Add task")
-            print("6) Delete task")
-            print("0) Exit\n")
+            print(Style.BRIGHT + Fore.LIGHTBLUE_EX + TITLE)
+            print("1] Today's tasks")
+            print("2] Week's tasks")
+            print("3] All tasks")
+            print("4] Missed tasks")
+            print("5] Add task")
+            print("6] Delete task")
+            print("0] Exit\n")
 
             try:
-                user_input = int(input())
+                user_input = int(input('Action: '))
                 print()
             except ValueError as ve:
                 print(ve)
@@ -130,7 +141,7 @@ class TodoList:
                 if action is not None:
                     action()
                 else:
-                    print('Bye!')
+                    print('Bye👋!')
                     break
 
 
